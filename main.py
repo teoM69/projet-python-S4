a8d3f31ea02fd1a887d12e3d6f0dd8d857fd9f0d
Teo <teo.menuel@gmail.com>
2026-03-27 11:16:03 +0100

    liaison de lobby.py et main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 from code.game import Game
+from code.lobby import Lobby
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 700))
 game = Game(screen)
+lobby = Lobby(screen)
 running = True
 while running:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    game.world.drawBackGround(screen)
-    game.world.drawWalls(screen)
+    if lobby.inMenu:
+        lobby.showMenu(screen)
+        lobby.run(screen)
+    else:
+        game.world.drawBackGround(screen)
+        game.world.drawWalls(screen)
+
     pygame.display.flip()
 pygame.quit()
