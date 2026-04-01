d078c3ca1c543a076eebc65560046853c9615482
Mgt689 <Papa-Maguette_Diop@etu.u-bourgogne.fr>
2026-04-01 13:53:59 +0200

    correction incoherence

--- code/Obstacle.py
+++ code/Obstacle.py
@@ -1,12 +1,11 @@
 import pygame
 
 class Obstacle:
-        def __init__(self, x, y, obstacleType, speed):
+        def __init__(self, x, y, obstacleType):
             self.coord = pygame.math.Vector2(x,y)
             self.type= obstacleType
             self.image = pygame.Surface((50, 50))
             self.image.fill((255, 0, 0))
-            self.rect = self.image.get_rect(topleft=(x, y))
         def move(self, speed):
             self.coord.x -= speed
             self.rect.x = self.coord.x
